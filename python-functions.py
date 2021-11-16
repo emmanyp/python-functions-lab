2a48b5a064ef7159a9707381a06bb742f032b70f
emma <emma.ronaldo7@gmail.com>
2021-11-16 13:26:44 -0500

    Challenge 3 done

--- python-functions.py
+++ python-functions.py
@@ -7,7 +7,6 @@ print(sum_to(10))
 
 def largest(list):
     max = list[0]
-
     for n in list:
         if n > max:
             max = n
@@ -16,3 +15,13 @@ def largest(list):
 
 print(largest([1, 2, 3, 4, 0]))
 print(largest([10, 4, 2, 231, 91, 54]))
+
+
+def occurances(str1, str2):
+  print(str1.count(str2))
+
+
+occurances('fleep floop', 'e')   # returns 2
+occurances('fleep floop', 'p')   # returns 2
+occurances('fleep floop', 'oo')  # returns 1
+occurances('fleep floop', 'fe')  # returns 0
