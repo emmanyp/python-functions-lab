c731b617572a60a5d0f6bc18ea8313009c409089
emma <emma.ronaldo7@gmail.com>
2021-11-15 23:07:44 -0500

    Challenge 2 done

--- python-functions.py
+++ python-functions.py
@@ -3,3 +3,16 @@ def sum_to(n):
 
 print(sum_to(6)) 
 print(sum_to(10)) 
+
+
+def largest(list):
+    max = list[0]
+
+    for n in list:
+        if n > max:
+            max = n
+    return max
+
+
+print(largest([1, 2, 3, 4, 0]))
+print(largest([10, 4, 2, 231, 91, 54]))
